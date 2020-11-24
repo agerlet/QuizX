005ad66b37db9638187662cc52279cfeef2438f8
Simon Feng <agerlet@gmail.com>
2020-11-25 00:55:09 +1100

    - Introduce the drag n drop feature. - Hardcoded the question.

---
+++ README.md
@@ -0,0 +1,90 @@
+# What is xed?
+
+The xed is a tool to help education.
+
+The xed started from a simple idea/question: Choose the right words to complete the dialogue.
+
+In the beginning, it's just a paragraph with some words being pulled out. As the replacements, some textboxes are in the positions of the missing words.
+
+Then, in the second iteration, the textboxes are replaced by the draggable boxes so that the user can drag the text into the paragraph.
+
+The third iteration, the whole page should be able to work with the devices with touch screen. 
+
+## Architecture
+
+The xed comes with 3 parts: 
+- the service 
+- student portal
+- teacher portal 
+
+### The service
+
+The service is a dotnet core 3.1 WebAPI running in the background to collect data from student portal and get ready to be queried by the teacher portal.
+
+#### Restore
+
+    # At the root of this repository, run the following command
+    $ dotnet restore
+
+#### Build
+
+    # At the root of this repository, run the following command
+    $ dotnet build
+
+#### Test
+
+    # At the root of this repository, run the following command
+    $ dotnet test
+    
+#### Run
+    
+    ## At the root of this repository, run the following command
+    $ dotnet run --project ./api/api.csproj
+
+### The student portal
+
+The student portal is a react project to present the quiz to the students and report the student activities.
+
+#### Install
+
+    # At the student directory of this repository, run the following command
+    $ yarn install
+
+#### Build
+
+    # At the student directory of this repository, run the following command
+    $ yarn build
+
+#### Test
+
+    # At the student of this repository, run the following command
+    $ yarn test
+    
+#### Run
+    
+    ## At the student of this repository, run the following command
+    $ yarn start
+
+### The teacher portal
+
+The teacher portal is a react project to present the answers to the teacher.
+
+#### Install
+
+    # At the teacher directory of this repository, run the following command
+    $ yarn install
+
+#### Build
+
+    # At the teacher directory of this repository, run the following command
+    $ yarn build
+
+#### Test
+
+    # At the teacher of this repository, run the following command
+    $ yarn test
+    
+#### Run
+    
+    ## At the teacher of this repository, run the following command
+    $ yarn start
--- student/src/App.tsx
+++ student/src/App.tsx
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+//@ts-ignore
+import {DragDropContainer, DropTarget} from 'react-drag-drop-container';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [blanks, setBlanks] = useState<any[]>(['','','','','','']);
+
+    function handleDrop(e: any) {
+        setBlanks(_ => {
+            _[e.dropData.id] = e.dragData.label;
+            return [..._];
+        });
+    }
+
+    return (
+        <div className="quiz">
+            <ul className="options">
+                <DragDropContainer
+                    targetKey="blank"
+                    dragData={{label: "凉凉"}}
+                    onDrop={handleDrop}
+                >
+                    <li className="item">
+                        凉凉
+                    </li>
+                </DragDropContainer>
+                <DragDropContainer
+                    targetKey="blank"
+                    dragData={{label: "雪花"}}
+                    onDrop={handleDrop}
+                >
+                    <li className="item">
+                        雪花
+                    </li>
+                </DragDropContainer>
+                <DragDropContainer
+                    targetKey="blank"
+                    dragData={{label: "尝一尝"}}
+                    onDrop={handleDrop}
+                >
+                    <li className="item">
+                        尝一尝
+                    </li>
+                </DragDropContainer>
+                <DragDropContainer
+                    targetKey="blank"
+                    dragData={{label: "变成"}}
+                    onDrop={handleDrop}
+                >
+                    <li className="item">
+                        变成
+                    </li>
+                </DragDropContainer>
+                <DragDropContainer
+                    targetKey="blank"
+                    dragData={{label: "甜"}}
+                    onDrop={handleDrop}
+                >
+                    <li className="item">
+                        甜
+                    </li>
+                </DragDropContainer>
+            </ul>
+            <div className="body">
+                <p>
+                    亮亮：冬冬快看！下雪了！
+                </p>
+                <p>
+                    冬冬：
+                    <DropTarget
+                        targetKey="blank"
+                        dropData={{id: 0}}
+                    >
+                        <span className="blank">
+                            {blanks[0]}
+                        </span>
+                    </DropTarget>
+                    白白的，真好看！
+                </p>
+                <p>
+                    亮亮：你知道雪是什么
+                    <DropTarget
+                        targetKey="blank"
+                        dropData={{id: 1}}
+                    >
+                        <span className="blank">
+                            {blanks[1]}
+                        </span>
+                    </DropTarget>
+                    的吗？
+                </p>
+                <p>
+                    冬冬：是水，对吗？
+                </p>
+                <p>
+                    亮亮：对！雪
+                    <DropTarget
+                        targetKey="blank"
+                        dropData={{id: 2}}
+                    >
+                        <span className="blank">
+                            {blanks[2]}
+                        </span>
+                    </DropTarget>吗？
+                </p>
+                <p>
+                    冬冬：我们
+                    <DropTarget
+                        targetKey="blank"
+                        dropData={{id: 3}}
+                    >
+                        <span className="blank">
+                            {blanks[3]}
+                        </span>
+                    </DropTarget>就知道了。
+                </p>
+                <p>
+                    亮亮：雪不
+                    <DropTarget
+                        targetKey="blank"
+                        dropData={{id: 4}}
+                    >
+                        <span className="blank">
+                            {blanks[4]}
+                        </span>
+                    </DropTarget>也不咸，吃在嘴里
+                    <DropTarget
+                        targetKey="blank"
+                        dropData={{id: 5}}
+                    >
+                        <span className="blank">
+                            {blanks[5]}
+                        </span>
+                    </DropTarget>
+                    的。
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
