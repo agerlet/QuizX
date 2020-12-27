770c93edebd42b1139170dfee4f238efbfa5f1b7
agerlet <agerlet@gmail.com>
2020-12-27 12:55:17 +1100

    Make the properties in QuizAnswerModel as camelCase for JSON object

--- scripts/start-dynamodb-local.sh
+++ scripts/start-dynamodb-local.sh
@@ -4,9 +4,9 @@ docker run -p 8000:8000 -d amazon/dynamodb-local -jar DynamoDBLocal.jar -sharedD
 
 aws dynamodb create-table \
   --table-name QuizX \
-  --attribute-definitions AttributeName=QuizId,AttributeType=S \
-                          AttributeName=StudentId,AttributeType=S \
-  --key-schema AttributeName=QuizId,KeyType=HASH \
-               AttributeName=StudentId,KeyType=RANGE \
+  --attribute-definitions AttributeName=quizId,AttributeType=S \
+                          AttributeName=studentId,AttributeType=S \
+  --key-schema AttributeName=quizId,KeyType=HASH \
+               AttributeName=studentId,KeyType=RANGE \
   --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
   --endpoint-url=http://localhost:8000
